c9590a738515b267d6facf65d9b0f2cc5eec947b
Alvin Xu <alvin3xu1@gmail.com>
2023-07-18 23:53:49 -0700

    init

---
+++ src/main/java/com/alvinxu/TheDailyGrind/TheDailyGrindApplication.java
@@ -0,0 +1,13 @@
+package com.alvinxu.TheDailyGrind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDailyGrindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDailyGrindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvinxu/TheDailyGrind/TheDailyGrindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvinxu.TheDailyGrind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDailyGrindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
