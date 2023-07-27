4b1f6954c0ed4208cca94ceff36908a938f4d287
Alvin Xu <alvin3xu1@gmail.com>
2023-07-27 00:22:14 -0700

    cleaning out home

--- src/main/java/com/alvinxu/TheDailyGrind/repositories/AccountRepository.java
+++ src/main/java/com/alvinxu/TheDailyGrind/repositories/AccountRepository.java
@@ -1,6 +1,9 @@
 package com.alvinxu.TheDailyGrind.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alvinxu.TheDailyGrind.models.Account;
@@ -8,4 +11,10 @@ import com.alvinxu.TheDailyGrind.models.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	public Account findByEmail(String email);
+	
+	@Query(value="SELECT * FROM account a WHERE a.username LIKE :username"
+	    + " ORDER BY LOCATE(:username, a.username), a.username ASC",
+	    nativeQuery=true
+	)
+	public List<Account> findSimilarToUsername(String username);
 }
