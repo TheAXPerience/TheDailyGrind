b8cf45fca4ce32d905642a3b1aa341d6e064dda2
Alvin Xu <alvin3xu1@gmail.com>
2023-08-02 18:26:06 -0700

    finished tests in models

--- src/test/java/com/alvinxu/TheDailyGrind/models/TestCalendarEvent.java
+++ src/test/java/com/alvinxu/TheDailyGrind/models/TestCalendarEvent.java
@@ -1,5 +1,129 @@
 package com.alvinxu.TheDailyGrind.models;
 
-public class TestCalendarEvent {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCalendarEvent {
+  @Test
+  void testId() {
+    // with
+    CalendarEvent event = new CalendarEvent();
+    
+    // when
+    event.setId(2L);
+    
+    // then
+    assertThat(event.getId()).isEqualTo(2L);
+  }
+  
+  @Test
+  void testEventOrganizer() {
+ // with
+    CalendarEvent event = new CalendarEvent();
+    Account user = new Account(
+        1L,
+        "hello@world",
+        "username",
+        "password",
+        LocalDateTime.of(2021, 5, 20, 4, 20),
+        "USER"
+    );
+    
+    // when
+    event.setEventOrganizer(user);
+    
+    // then
+    // this is why we test Account.equals()
+    assertThat(event.getEventOrganizer()).isEqualTo(user);
+  }
+  
+  @Test
+  void testDateCreated() {
+ // with
+    CalendarEvent event = new CalendarEvent();
+    LocalDateTime date = LocalDateTime.of(2022, 3, 17, 2, 49);
+    
+    // when
+    event.setDateCreated(date);
+    
+    // then
+    assertThat(event.getDateCreated()).isEqualTo(date);
+  }
+  
+  @Test
+  void testDateUpdated() {
+ // with
+    CalendarEvent event = new CalendarEvent();
+    LocalDateTime date = LocalDateTime.of(2022, 3, 17, 2, 49);
+    
+    // when
+    event.setDateUpdated(date);
+    
+    // then
+    assertThat(event.getDateUpdated()).isEqualTo(date);
+  }
+  
+  @Test
+  void testDateOfEvent() {
+ // with
+    CalendarEvent event = new CalendarEvent();
+    LocalDateTime date = LocalDateTime.of(2022, 3, 17, 2, 49);
+    
+    // when
+    event.setDateOfEvent(date);
+    
+    // then
+    assertThat(event.getDateOfEvent()).isEqualTo(date);
+  }
+  
+  @Test
+  void testTitle() {
+ // with
+    CalendarEvent event = new CalendarEvent();
+    String title = "hello world";
+    
+    // when
+    event.setTitle(title);
+    
+    // then
+    assertThat(event.getTitle()).isEqualTo(title);
+  }
+  
+  @Test
+  void testDescription() {
+ // with
+    CalendarEvent event = new CalendarEvent();
+    String description = "this is the description of an event. ::)))";
+    
+    // when
+    event.setDescription(description);
+    
+    // then
+    assertThat(event.getDescription()).isEqualTo(description);
+  }
+  
+  @Test
+  void testIsPublic() {
+    CalendarEvent event = new CalendarEvent();
+    
+    event.setPublic(true);
+    assertThat(event.isPublic()).isTrue();
+    
+    event.setPublic(false);
+    assertThat(event.isPublic()).isFalse();
+  }
+  
+  @Test
+  void testIsComplete() {
+    CalendarEvent event = new CalendarEvent();
+    
+    event.setComplete(true);
+    assertThat(event.isComplete()).isTrue();
+    
+    event.setComplete(false);
+    assertThat(event.isComplete()).isFalse();
+  }
 }
--- src/test/java/com/alvinxu/TheDailyGrind/models/TestDiaryEntry.java
+++ src/test/java/com/alvinxu/TheDailyGrind/models/TestDiaryEntry.java
@@ -1,5 +1,79 @@
 package com.alvinxu.TheDailyGrind.models;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
 public class TestDiaryEntry {
+  @Test
+  void testId() {
+    DiaryEntry entry = new DiaryEntry();
+    
+    entry.setId(5L);
+    assertThat(entry.getId()).isEqualTo(5L);
+  }
+  
+  @Test
+  void testDiaryOwner() {
+    DiaryEntry entry = new DiaryEntry();
+    Account user = new Account(
+        6L,
+        "rebecca@black",
+        "Rebecca Black",
+        "rebeccaBlack<3<3<3",
+        LocalDateTime.of(2012, 12, 29, 5, 45),
+        "USER"
+    );
+    
+    entry.setDiaryOwner(user);
+    assertThat(entry.getDiaryOwner()).isEqualTo(user);
+  }
+  
+  @Test
+  void testDateCreated() {
+    DiaryEntry entry = new DiaryEntry();
+    LocalDateTime date = LocalDateTime.of(2022, 3, 17, 2, 49);
+    
+    entry.setDateCreated(date);
+    assertThat(entry.getDateCreated()).isEqualTo(date);
+  }
+  
+  @Test
+  void testDateUpdated() {
+    DiaryEntry entry = new DiaryEntry();
+    LocalDateTime date = LocalDateTime.of(2022, 3, 17, 2, 49);
+    
+    entry.setDateUpdated(date);
+    assertThat(entry.getDateUpdated()).isEqualTo(date);
+  }
+  
+  @Test
+  void testDateOfEntry() {
+    DiaryEntry entry = new DiaryEntry();
+    LocalDateTime date = LocalDateTime.of(2022, 3, 17, 2, 49);
+    
+    entry.setDateOfEntry(date);
+    assertThat(entry.getDateOfEntry()).isEqualTo(date);
+  }
+  
+  @Test
+  void testTitle() {
+    DiaryEntry entry = new DiaryEntry();
+    String title = "title of the diary entry";
+    
+    entry.setTitle(title);
+    assertThat(entry.getTitle()).isEqualTo(title);
+  }
+  
+  @Test
+  void testEntry() {
+    DiaryEntry entry = new DiaryEntry();
+    String entryText = "omg have you SEEN justin's shoes? they're to DIE for~";
+    
+    entry.setEntry(entryText);
+    assertThat(entry.getEntry()).isEqualTo(entryText);
+  }
 
 }
